c319a91a30b73996436a4b9fe09b2236bc7865de
gdubus <gdubus@norauto.com>
2020-03-04 20:39:12 +0100

    Improve style of login & forgotten password pages

--- cc/settings.py
+++ cc/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'widget_tweaks',
+
     'backendapi',
     'frontendjs',
 ]
@@ -126,5 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "cc/static"),
+]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
